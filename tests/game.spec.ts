41e334d9674f5b25871c6c246d75d8b26d061a9a
amir.reich <amir.reich@wsc-sports.com>
2024-02-25 12:56:35 +0200

    hello world

--- tests/game.spec.ts
+++ tests/game.spec.ts
@@ -8,24 +8,3 @@ test.beforeEach(async ({ page }: { page: Page}) => {
 test('Hello World', async ({ page }: { page: Page}) => {
   await expect(page).toHaveTitle(/Harry Potter Quest/);
   });
-
-test('Should finish the game', async ({ page }: { page: Page}) => {
-    await page.getByTestId('btnStart').click();
-
-    await expect(page.getByTestId('score')).toHaveText('10');
-
-    await page.getByPlaceholder('Enter the spell').fill('Leviosaaa');
-
-    await expect(page.getByTestId('score')).toHaveText('20');
-
-    await page.getByLabel('Train Station').selectOption({ label: 'Kings Cross 9 3/4' });
-
-    await expect(page.getByTestId('score')).toHaveText('30');
-
-    const stinkySock = page.getByTestId('stinkySock');
-    const dwarf = page.getByTestId('dobby');
-
-    await stinkySock.dragTo(dwarf);
-
-    await expect(page.getByText('Fin')).toBeVisible();
-});
